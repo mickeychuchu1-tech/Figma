cd04c51694bd2a2a37bfa7b3098fdd7a2cc3d6ed
mickeychuchu1-tech <mickeychuchu1@gmail.com>
2026-05-16 17:34:24 +0100

    Update link to details page in Fetchdata function

--- Recipes.js
+++ Recipes.js
@@ -13,7 +13,7 @@ const Fetchdata=async()=>{
         const div=document.createElement("div")
          div.onclick=()=>{
             const a=document.createElement("a")
-            a.href=`C:/Users/ADMIN/Desktop/Figma/details.html?id=${item.idMeal}`
+            a.href=`https://figma-two-omega.vercel.app/details.html?id=${item.idMeal}`
             a.click()
         }
         div.style.width="300px"
